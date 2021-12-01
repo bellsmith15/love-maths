660ceb4c08b168a8f3014e05ea16d2aa5d2348c6
bellsmith15 <smith27n@gmail.com>
2021-12-01 16:24:59 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,24 +3,46 @@
 
 document.addEventListener("DOMContentLoaded",function (){
     let buttons = document.getElementsByTagName("button")
-
     // for (let i = 0; i <<button.length; i++ ) OLD way of doing it
-
     for (let button of buttons) {
         button.addEventListener("click", function () {
             if (this.getAttribute("data-type") === "submit") {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                // alert(`You clicked ${gameType}`); to run the game see below the parameter is "gameType"
+                runGame(gameType);
             }
         })
-    }   
+    }  
+    
+    runGame("addition");
+
+
 })
 
-function runGame (){
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+ */
+function runGame (gameType) {
+
+    // Creates two random nummbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`unknown game type: ${gameType}`);
+        throw `Unkonwn game type: ${gameType}.Aborting!`;
+    }
+
+
 
 }
+
+// runGame();  This pops up the description for the DOcstring in JS so you know the description without having to search for where it was placed in the script
 
 function checkAnswer () {
 
@@ -34,9 +56,12 @@ function incrementWrongAnswer () {
 
 }
 
-function displayAdditionQuestion () {
-
+function displayAdditionQuestion (operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 }
+
 
 function displaySubtractQuestion () {
     
