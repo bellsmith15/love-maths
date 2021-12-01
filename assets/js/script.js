88294276e9d24b031ce83c0a9dedb2464aa25701
bellsmith15 <smith27n@gmail.com>
2021-12-01 15:20:16 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame (){
+
+}
+
+function checkAnswer () {
+
+}
+
+function incrementScore () {
+
+}
+
+function incrementWrongAnswer () {
+
+}
+
+function displayAdditionQuestion () {
+
+}
+
+function displaySubtractQuestion () {
+    
+}
+
+function displaMultiplyQuestion () {
+    
+}
+
+// TEST one for me to do DIVIDE function displayDivideQuestion () {
+    
+// }
